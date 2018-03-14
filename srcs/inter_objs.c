965046b35d6b11c2310ff979fdbe98d2da03edcd
Benjamin MUSELET <bmuselet@e3r11p13.42.fr>
2018-03-14 10:56:58 +0100

    trying new cone inter

--- srcs/inter_objs.c
+++ srcs/inter_objs.c
@@ -152,14 +152,24 @@ double	cone_inter(t_env *e, t_ray *ray)
 	double	r;
 	t_vec	temp;
 
+	//ocos[x] = cos(ft_deg2rad(((t_obj*)e->objs->content)->rad));
+	//osin[x] = sin(ft_deg2rad(((t_obj*)e->objs->content)->rad));
 	temp = vector_substraction(e->cam.pos, ((t_obj*)e->objs->content)->pos);
-	r = (double)((t_obj*)e->objs->content)->rad * (double)M_PI / 180.0;
+	/*r = (double)((t_obj*)e->objs->content)->rad * (double)M_PI / 180.0;
 	a.x = pow(ray->dir.x, 2) + pow(ray->dir.y, 2) -
 		pow(ray->dir.z, 2) * pow(tan(r), 2);
 	a.y = (2 * temp.x * ray->dir.x) + ( 2 * temp.y * ray->dir.y) -
 		(2 * temp.z * ray->dir.z) * pow(tan(r), 2);
 	a.z = pow(temp.x, 2) + pow(temp.y, 2) -
-		pow(temp.z, 2) * pow(tan(r), 2);
+		pow(temp.z, 2) * pow(tan(r), 2);*/
+	r = pow(cos(ft_deg2rad(((t_obj*)e->objs->content)->rad)) /
+		sin(ft_deg2rad(((t_obj*)e->objs->content)->rad)), 2);
+	a.x = pow(ray->dir.x, 2) * r + pow(ray->dir.y, 2) * r -
+    pow(ray->dir.z, 2);
+	a.y = 2.0 * (temp.x * ray->dir.x * r + temp.y * ray->dir.y * r -
+		temp.z * ray->dir.z);
+	a.z = pow(temp.x, 2) * r + pow(temp.y, 2) * r -
+		pow(temp.z, 2);
 	if ((new_length = equation_second(a, b)) == -1)
 		return (ray->length);
 	return (new_length);
