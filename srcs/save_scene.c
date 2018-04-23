00283767d8866486d47b639cc7372d2ebb8c5d23
MateoGreil <mateo.greil@gmail.com>
2018-04-23 12:14:47 +0200

    norme

--- srcs/save_scene.c
+++ srcs/save_scene.c
@@ -6,7 +6,7 @@
 /*   By: mgreil <mgreil@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/22 13:58:50 by mgreil            #+#    #+#             */
-/*   Updated: 2018/04/17 13:48:28 by mgreil           ###   ########.fr       */
+/*   Updated: 2018/04/23 12:14:32 by mgreil           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,20 +28,13 @@ void		cam_to_xml(t_cam cam, xmlNodePtr cur)
 
 static void	objs_to_xml_pt2(xmlNodePtr cur, t_list *obj)
 {
-	if ((!xmlStrcmp(cur->name, (const xmlChar *)"direction")))
-	{
+	if ((!xmlStrcmp(cur->name, (const xmlChar *)"rad")) ||
+	(!xmlStrcmp(cur->name, (const xmlChar *)"intensity")))
+		xmlNodeSetContent(cur, (xmlChar*)ft_itoa(((t_obj*)obj->content)->rad));
+	else if ((!xmlStrcmp(cur->name, (const xmlChar *)"direction")))
 		xml_set_vec(cur->xmlChildrenNode, ((t_obj*)obj->content)->dir);
-	}
 	else if ((!xmlStrcmp(cur->name, (const xmlChar *)"color")))
-	{
 		xml_set_color(cur->xmlChildrenNode, ((t_obj*)obj->content)->color);
-	}
-	/*else if ((!xmlStrcmp(cur->name, (const xmlChar *)"reflection")))
-		xml_set_type(cur->xmlChildrenNode, ((t_obj*)obj->content)->refl);
-	else if ((!xmlStrcmp(cur->name, (const xmlChar *)"refraction")))
-		xml_set_type(cur->xmlChildrenNode, ((t_obj*)obj->content)->refr);
-	else if ((!xmlStrcmp(cur->name, (const xmlChar *)"n_refr")))
-		xml_set_type(cur->xmlChildrenNode, ((t_obj*)obj->content)->n_refr);*/
 }
 
 void		objs_to_xml(t_list *objs, xmlNodePtr objs_xml)
@@ -56,28 +49,14 @@ void		objs_to_xml(t_list *objs, xmlNodePtr objs_xml)
 		while (cur)
 		{
 			if ((!xmlStrcmp(cur->name, (const xmlChar *)"type")))
-			{
 				xml_set_type(cur->xmlChildrenNode,
 					((t_obj*)obj->content)->type);
-			}
 			else if ((!xmlStrcmp(cur->name, (const xmlChar *)"position")))
-			{
 				xml_set_vec(cur->xmlChildrenNode, ((t_obj*)obj->content)->pos);
-			}
 			else if ((!xmlStrcmp(cur->name, (const xmlChar *)"rotation")))
-			{
 				xml_set_vec(cur->xmlChildrenNode, ((t_obj*)obj->content)->rot);
-			}
 			else if ((!xmlStrcmp(cur->name, (const xmlChar *)"translation")))
-			{
 				xml_set_vec(cur->xmlChildrenNode, (t_vec){0, 0, 0, 0});
-			}
-			else if ((!xmlStrcmp(cur->name, (const xmlChar *)"rad")) ||
-			(!xmlStrcmp(cur->name, (const xmlChar *)"intensity")))
-			{
-				xmlNodeSetContent(cur,
-					(xmlChar*)ft_itoa(((t_obj*)obj->content)->rad));
-			}
 			objs_to_xml_pt2(cur, obj);
 			cur = cur->next;
 		}
@@ -99,11 +78,7 @@ void		struct_to_xml(t_env *e)
 		if ((!xmlStrcmp(cur->name, (const xmlChar *)"cam")))
 			cam_to_xml(e->cam, cur->xmlChildrenNode);
 		else if ((!xmlStrcmp(cur->name, (const xmlChar *)"objs")))
-		{
 			objs_to_xml(e->objs, cur->xmlChildrenNode);
-		}
-		//else if ((!xmlStrcmp(cur->name, (const xmlChar *)"lights")))
-		//	lights_to_xml(e->lights, e->doc);
 		cur = cur->next;
 	}
 }
